a1f9b989fa78758d41ccba057e4b10675b28f95b
AYMANE <aymanesabilallah01@gmail.com>
2026-05-19 01:28:17 +0100

    modification for hosting render

--- Backend/shared/database/database.py
+++ Backend/shared/database/database.py
@@ -1,7 +1,8 @@
 import os
 import aiomysql
 from dotenv import load_dotenv
-
+import ssl
+ssl_context = ssl.create_default_context(cafile="/certs/isrgrootx1.pem")
 load_dotenv(override=True)
 class Database:
     async def create_pool(self):
@@ -12,6 +13,7 @@ class Database:
             db=os.getenv("DB"),
             minsize=5,
             maxsize=32,
+            ssl=ssl_context,
             cursorclass=aiomysql.DictCursor,
             init_command="SET time_zone = '+00:00'"
         )
