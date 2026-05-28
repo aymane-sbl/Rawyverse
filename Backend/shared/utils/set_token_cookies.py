ad23bdb068ebbac1aa3e51276937fef7107a0fac
AYMANE SABIL ALLAH <aymanesabilallah01@gmail.com>
2026-05-28 18:21:34 +0100

    Update set_token_cookies.py
    update tioe for delete token from cookies

--- Backend/shared/utils/set_token_cookies.py
+++ Backend/shared/utils/set_token_cookies.py
@@ -2,6 +2,6 @@ from shared.utils.securite import encode_token
 from datetime import datetime,timedelta,timezone
 
 def create_token_cookies(email,role,response):
-    payload = {"sub": email,"role":role, "exp": datetime.now(timezone.utc) + timedelta(minutes=20)}
+    payload = {"sub": email,"role":role, "exp": datetime.now(timezone.utc) + timedelta(days=30)}
     token = encode_token(payload)
-    response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite="none", max_age=1800)
+    response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite="none", max_age= 30* 24 * 60 * 60)
