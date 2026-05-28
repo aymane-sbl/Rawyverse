a0509ad43cd3d4c7198f7bc6262f85a0edca5be8
AYMANE SABIL ALLAH <aymanesabilallah01@gmail.com>
2026-05-28 19:31:37 +0100

    Update validator.js
    Update display for validator

--- Frontend/js/views/auth/utils/validator.js
+++ Frontend/js/views/auth/utils/validator.js
@@ -1,7 +1,7 @@
 export function validator(input,id,regex_pattern) {
     if(input === "" || regex_pattern.test(input) === false){
-            document.querySelector(id).style.visibility = "visible";
+            document.querySelector(id).style.display = "block";
         }else{
-            document.querySelector(id).style.visibility = "hidden";
+            document.querySelector(id).style.display = "none";
         }
 }
