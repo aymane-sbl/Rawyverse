a8a13954307d0abe5745ac452d6affc63d5fcc73
AYMANE SABIL ALLAH <aymanesabilallah01@gmail.com>
2026-05-28 14:02:27 +0100

    Update set_token_cookies.py
    update samsite ton none

--- Backend/shared/utils/set_token_cookies.py
+++ Backend/shared/utils/set_token_cookies.py
@@ -4,4 +4,4 @@ from datetime import datetime,timedelta,timezone
 def create_token_cookies(email,role,response):
     payload = {"sub": email,"role":role, "exp": datetime.now(timezone.utc) + timedelta(minutes=20)}
     token = encode_token(payload)
-    response.set_cookie(key="access_token", value=token, httponly=True, secure=False, samesite="lax", max_age=1800)
+    response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite="none", max_age=1800)
