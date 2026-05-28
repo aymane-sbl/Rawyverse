f3fb5664aa71b906608d3e21e0392c7297648a4b
AYMANE SABIL ALLAH <aymanesabilallah01@gmail.com>
2026-05-28 12:55:42 +0100

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -28,7 +28,8 @@ async def lifespan(app: FastAPI):
     pool =await db.create_pool()
     app.state.pool = pool
 #     init redis
-    redis = redis_as.from_url(os.getenv("REDIS_URL"),decode_responses=True)
+        redis = redis_as.from_url(os.getenv("REDIS_URL"), decode_responses=True, ssl_cert_reqs="none")
+
     app.state.redis = redis
 #   init redis cache
     init_redis_cache(redis=redis, redis_backend=RedisBackend ,fast_api_cache=FastAPICache)
