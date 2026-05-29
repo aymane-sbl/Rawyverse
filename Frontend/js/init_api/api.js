3c10f709a4e2702a372884c6889889c6d66da213
AYMANE SABIL ALLAH <aymanesabilallah01@gmail.com>
2026-05-29 03:29:26 +0100

    Update api.js

--- Frontend/js/init_api/api.js
+++ Frontend/js/init_api/api.js
@@ -2,7 +2,10 @@
 
 export async function initApi(endpoints,options={method : "GET",credentials : "include",cache : "no-store"}) {
     const baseUrl ="https://api.rawyverse.xyz";
+    let container =
+document.querySelector(".container");
     GlobalLoader("block");
+     container.innerHTML = "";
    try{
       let response = await fetch(`${baseUrl}${endpoints}`,options);
        if (!response.ok){
